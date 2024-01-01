efb37ceef915de0902cd67995684ad870642eabf
Raghav Bali <raghavbali96@yahoo.com>
2024-01-01 13:23:48 +0530

    Refactor code to iterate over each row in the dataframe and send it to the ML endpoint

--- MLConsumeApp.py
+++ MLConsumeApp.py
@@ -4,8 +4,8 @@ import os
 import ssl
 import ClassSession
 import csv
-import config  # import the config module
 import pandas as pd
+import config # config.py
 
 def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
@@ -14,47 +14,51 @@ def allowSelfSignedHttps(allowed):
 
 allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
 
-# Open and read the csv and parse its contents
+# Open and read the csv and parse its contents into a pandas dataframe
 csvUrl = config.csvUrl
 df = pd.read_csv(csvUrl)
 wrapper = ClassSession.ClassSessionWrapper(df)
-print(wrapper.get_row(0).tolist())
 
-# Request data goes here
-# The example below assumes JSON formatting which may be updated
-# depending on the format your endpoint expects.
-# More information can be found here:
-# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
-data =  {
-  "input_data": {
-    "columns": wrapper.get_columns(),
-    "index": [0],
-    "data": [wrapper.get_row(0).tolist()]
+# Iterate over each row in the dataframe and send it to the endpoint
+for index, row in df.iterrows():
+  # Request data goes here
+  # The example below assumes JSON formatting which may be updated
+  # depending on the format your endpoint expects.
+  # More information can be found here:
+  # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+  data =  {
+    "input_data": {
+      "columns": wrapper.get_columns(),
+      "index": [index],
+      "data": [wrapper.get_row(index).tolist()]
+    }
   }
-}
 
-body = str.encode(json.dumps(data))
+  body = str.encode(json.dumps(data))
 
-url = 'https://barriodanceml-jebdj.centralus.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key or AMLToken for the endpoint
-api_key = config.api_key  # use the api_key from config
-if not api_key:
-    raise Exception("A key should be provided to invoke the endpoint")
+  url = config.mlEndpointUrl
+  # Replace this with the primary/secondary key or AMLToken for the endpoint
+  api_key = config.api_key
+  if not api_key:
+      raise Exception("A key should be provided to invoke the endpoint")
 
-# The azureml-model-deployment header will force the request to go to a specific deployment.
-# Remove this header to have the request observe the endpoint traffic rules
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'barriodance2023classes-1' }
+  # The azureml-model-deployment header will force the request to go to a specific deployment.
+  # Remove this header to have the request observe the endpoint traffic rules
+  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'barriodance2023classesv2-1' }
 
-req = urllib.request.Request(url, body, headers)
+  req = urllib.request.Request(url, body, headers)
 
-try:
-    response = urllib.request.urlopen(req)
+  try:
+      response = urllib.request.urlopen(req)
 
-    result = response.read()
-    print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
+      result = response.read()
+      output_str = result.decode('utf-8')  # Decode from bytes to string
+      output_num = float(output_str.strip('[]'))  # Remove brackets and convert to float  
+      print(output_num)
+      
+  except urllib.error.HTTPError as error:
+      print("The request failed with status code: " + str(error.code))
 
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(error.read().decode("utf8", 'ignore'))
+      # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+      print(error.info())
+      print(error.read().decode("utf8", 'ignore'))
